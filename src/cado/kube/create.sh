3ba55580c439c7c3cd7157d9993e26b302f07162
Jason Parker-Burlingham <jasonp@panix.com>
2017-12-09 23:42:27 -0500

    Pack up changes made to run on GKE.

--- src/cado/kube/create.sh
+++ src/cado/kube/create.sh
@@ -3,34 +3,50 @@
 set -e
 set -o pipefail
 
-echo "Not actually a script you ran run (yet)."
-exit 1
-
 ZONE=us-east1-b
-PROJECT=${PROJECT}
+PROJECT=always-be-factoring
 
-# After running Build.sh:
+### After running Build.sh:
+# docker tag cado-server gcr.io/${PROJECT}/cado-server
+# gcloud docker -- push gcr.io/${PROJECT}/cado-server
+# 
+# docker tag cado-client gcr.io/${PROJECT}/cado-client
+# gcloud docker -- push gcr.io/${PROJECT}/cado-client
 
-docker tag cado-server gcr.io/${PROJECT}/cado-server
-gcloud docker -- push gcr.io/${PROJECT}/cado-server
-
-docker tag cado-client gcr.io/${PROJECT}/cado-client
-gcloud docker -- push gcr.io/${PROJECT}/cado-client
-
-gcloud compute disks create \
-	--size=500GB \
-	--zone=${ZONE} \
-	cado-server-disk
-
-gcloud container clusters create cado-cluster \
+# We have to run on Skylake because that's the platform the binaries are built
+# on and for.
+gcloud beta container clusters create cado-cluster \
 	--image-type=COS \
 	--machine-type=n1-standard-8 \
+	--min-cpu-platform "Intel Skylake" \
 	--num-nodes=5 \
 	--zone=${ZONE}
+
+# TODO(jasonp):  Edit/create a namespace to run in with a CPU limit of 1000
+# mCPU.
+kubectl create -f - --namespace=default <<EOF
+apiVersion: v1
+kind: LimitRange
+metadata:
+  name: cpu-limit-range
+spec:
+  limits:
+  - default:
+      cpu: 1
+    defaultRequest:
+      cpu: 0.5
+    type: Container
+EOF
+
+# gcloud compute disks create \
+# 	--size=500GB \
+# 	--zone=${ZONE} \
+# 	cado-server-disk
 
 # TODO(jasonp):  Add cado-server-disk to this deployment.
 kubectl run cado-server \
 	--replicas=1 \
+	--requests='cpu=1' \
 	--image=gcr.io/${PROJECT}/cado-server
 
 kubectl expose deployment cado-server \
@@ -40,6 +56,7 @@ kubectl expose deployment cado-server \
 # 5 hosts * 8 cpus each == 40 replicas, if the server is negligible.
 kubectl run cado-client \
 	--image=gcr.io/${PROJECT}/cado-client \
+	--limits='cpu=1' \
 	--replicas=40
 
 # Set up the kubernetes proxy as its console is much improved.
