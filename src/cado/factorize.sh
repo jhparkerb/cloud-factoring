fb1cc3f04115537f8931ebc047fbc06a4e063ba9
Jason Parker-Burlingham <jasonp@panix.com>
2018-10-13 00:39:17 -0400

    Remove debugging call to tmux.

--- src/cado/factorize.sh
+++ src/cado/factorize.sh
@@ -29,7 +29,6 @@ do
 				--workdir=. \
 				--bindir=/pkg/cado/lib/cado-nfs-3.0.0 \
 				--server=http://dist:4242/
-	tmux new-window -n client-${num} "docker attach client-${num}; read"
 done
 
 docker run \
