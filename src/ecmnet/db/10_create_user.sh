ff1f55ce7ebd550517a15599083d943d905b62df
Jason Parker-Burlingham <jasonp@panix.com>
2019-05-01 22:42:18 -0400

    Deletes ecmnet as it's no longer being worked on.

--- src/ecmnet/db/10_create_user.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-set -e
-
-# createuser -S -D -R -l -e -w -U postgres ecmnet
-psql -U ecmnet -c "alter user ecmnet with password 'ecmpassword';"
-
-# createdb -e -O ecmnet -E UNICODE -U postgres ecmnet
